a63a654408dc252cf17ea6d06d00c0b9f1d2101f
Lore <bujhtdbx@inbox.ru>
2022-05-18 12:49:21 +0300

    commit1

--- main.cpp
+++ main.cpp
@@ -13,7 +13,6 @@ int main(int argc, char*argv[])
     int s = -1;
     modbus_t *ctx;
     modbus_mapping_t *mb_mapping;
-    modbus_mapping_t *mb_mapping2;
     int rc;
     int i;
     uint8_t *query;
@@ -34,25 +33,70 @@ int main(int argc, char*argv[])
     }
 
     for (i=0; i < sizeof(UT_ADDRESS_BUFF_TAB) / sizeof(uint16_t); i++) {
-        for (int j=0; j < NB_REGISTERS_BUFF[i]; j++){
+        for (int j = 0; j < NB_REGISTERS_BUFF[i]; j++) {
             mb_mapping->tab_registers[UT_ADDRESS_BUFF_TAB[i] + j] = UT_REGISTERS_TAB[i][j];
         }
     }
-
 
     s = modbus_tcp_listen(ctx, 1);
     modbus_tcp_accept(ctx, &s);
 
     for (;;) {
-
         do {
             rc = modbus_receive(ctx, query);
         } while (rc == 0);
-
         if (rc == -1 && errno != EMBBADCRC) {
             break;
         }
 
+        if (MODBUS_GET_INT16_FROM_INT8(query, header_length + 1) == 0x0D60) {
+            switch(query[header_length+7]) {
+                case 1:
+                    if (query[header_length+9] == 64 && !((mb_mapping->tab_registers[0x0D40]>>7) % 2)) {
+                        mb_mapping->tab_registers[0x0D40] += 1<<7;
+                    } else if (query[header_length+9] == 32 && (mb_mapping->tab_registers[0x0D40]>>7) % 2) {
+                        mb_mapping->tab_registers[0x0D40] -= 1<<7;
+                    }
+                    break;
+                case 2:
+                    if (query[header_length+9] == 64 && !((mb_mapping->tab_registers[0x0D40]>>5) % 2)) {
+                        mb_mapping->tab_registers[0x0D40] += 1<<5;
+                    } else if (query[header_length+9] == 32 && (mb_mapping->tab_registers[0x0D40]>>5) % 2) {
+                        mb_mapping->tab_registers[0x0D40] -= 1<<5;
+                    }
+                    break;
+                case 3:
+                    if (query[header_length+9] == 64 && !((mb_mapping->tab_registers[0x0D40]>>3) % 2)) {
+                        mb_mapping->tab_registers[0x0D40] += 1<<3;
+                    } else if (query[header_length+9] == 32 && (mb_mapping->tab_registers[0x0D40]>>3) % 2) {
+                        mb_mapping->tab_registers[0x0D40] -= 1<<3;
+                    }
+                    break;
+                case 4:
+                    if (query[header_length+9] == 64 && !((mb_mapping->tab_registers[0x0D41]>>2) % 2)) {
+                        mb_mapping->tab_registers[0x0D41] += 1<<2;
+                    } else if (query[header_length+9] == 32 && (mb_mapping->tab_registers[0x0D41]>>2) % 2) {
+                        mb_mapping->tab_registers[0x0D41] -= 1<<2;
+                    }
+                    break;
+                case 5:
+                    if (query[header_length+9] == 64 && !((mb_mapping->tab_registers[0x0D40]>>10) % 2)) {
+                        mb_mapping->tab_registers[0x0D40] += 1<<10;
+                    } else if (query[header_length+9] == 32 && !((mb_mapping->tab_registers[0x0D41]>>11) % 2)) {
+                        mb_mapping->tab_registers[0x0D41] -= 1<<11;
+                    }
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    if (query[header_length+9] == 64)
+                        for (i=0; i < 12; i++)
+                            mb_mapping->tab_registers[0x0CC0 + i] = 0;
+                    break;
+                case 8:
+                    break;
+            }
+        }
 
 
 
